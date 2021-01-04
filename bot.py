48750d3a8cf3056b15a9f31fd2ee99c5deacbf7e
pRiyaNshu bHaRdwaj <itispriyanshu@gmail.com>
2021-01-05 02:42:27 +0530

    Update bot.py
    Added Configs.

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("//TOKEN//", use_context=True)
+    updater = Updater("1526670685:AAHBccYsiuiKTvZ8dSrYnDS7n9A2iObhfSk", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -58,7 +58,10 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("piracy", piracy))
+    dp.add_handler(CommandHandler("about", about))
+    dp.add_handler(CommandHandler("papa", papa))
+    dp.add_handler(CommandHandler("easter", easter))
+    dp.add_handler(CommandHandler("eggs", eggs))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
